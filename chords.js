33b0fec5641a4ee69a212f306c816e7d3d3ff018
jameswhiteley <jwhiteley0@icloud.com>
2017-07-10 22:25:20 +0100

    init commit

---
+++ chords.js
@@ -0,0 +1,8 @@
+const scribble = require('scribbletune');
+let chords = scribble.clip({
+    notes: ['F#min', 'C#min', 'Dmaj', 'Bmin', 'Emaj', 'Amaj', 'Dmaj', 'C#min', 'Amaj'],
+    pattern: 'x_x_x_--'.repeat(8),
+    sizzle: true
+});  
+ 
+scribble.midi(chords, 'chords.mid');
