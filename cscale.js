8cfb46507c254e2a3e6a9a5a3fdd049164aa4155
jameswhiteley <jwhiteley0@icloud.com>
2017-07-11 15:23:40 +0100

    change single quotes to double quotes

--- cscale.js
+++ cscale.js
@@ -1,8 +1,8 @@
-const scribble = require('scribbletune');
+const scribble = require("scribbletune");
  
 let clip = scribble.clip({
-    notes: scribble.scale('c', 'major', 3), // this works too ['c3', 'd3', 'e3', 'f3', 'g3', 'a3', 'b3'] 
-    pattern: 'x-'.repeat(8)
+    notes: scribble.scale("c", "major", 3), // this works too ["c3", "d3", "e3", "f3", "g3", "a3", "b3"] 
+    pattern: "x-".repeat(8)
 });
  
-scribble.midi(clip, 'cscale.mid');
+scribble.midi(clip, "cscale.mid");
